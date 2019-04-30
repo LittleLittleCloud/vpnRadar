8dd3efc55efee7aa8c52e5d49407e19592fce45e
Xiaoyun Zhang <xiaoyuz@microsoft.com>
2019-04-30 15:00:58 -0700

    remove views in backend, and add graphQL

--- server/build/app.js
+++ server/build/app.js
@@ -1,18 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
 var testAPI_1 = require("./routes/testAPI");
-var cors = require("cors");
+var cors_1 = tslib_1.__importDefault(require("cors"));
 var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var app = express();
 // view engine setup
-app.set("views", path.join(__dirname, "../", "views"));
-app.set("view engine", "jade");
 //options for cors midddleware
 var options = {
     allowedHeaders: [
@@ -26,14 +22,11 @@ var options = {
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
     preflightContinue: false
 };
-app.use(cors(options));
+app.use(cors_1.default(options));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../", "public")));
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/testAPI", testAPI_1.testAPIrouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
